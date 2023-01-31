397fcc1732343c498380b960079ac97ce62f6ce7
TuranovDanil <turanoff.danil@yandex.ru>
2023-02-01 03:57:58 +0700

    добавлено свойство details и перебор этого массива в html

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ let app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inventory: 100,
         onSale: true,
-
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
 
 
     }
