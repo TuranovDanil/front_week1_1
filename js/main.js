0ee3a4460af1b7b53cb8c6cecea9162b265b0d12
TuranovDanil <turanoff.danil@yandex.ru>
2023-02-01 04:01:32 +0700

    добавлено свойство variants, выполнен вывод содержимого объекта в html

--- js/main.js
+++ js/main.js
@@ -9,6 +9,16 @@ let app = new Vue({
         inventory: 100,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ]
 
 
     }
