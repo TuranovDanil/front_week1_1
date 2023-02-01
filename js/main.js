5123fcc1583df01e1c9db9745409d8f8e82fec25
TuranovDanil <turanoff.danil@yandex.ru>
2023-02-01 19:19:25 +0700

    добавлено вычисляемое свойство title и image

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inventory: 5,
@@ -26,17 +27,27 @@ let app = new Vue({
 
 
     },
-    methods: {
-        addToCart(){
-            this.cart += 1
+    computed: {
+        title() {
+            return this.brand + " " + this.product;
         },
-        delToCart(){
-            if(this.cart > 0) {
-                this.cart -= 1
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        delToCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
             }
         },
-        updateProduct(variantImage){
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         }
+
     }
 })
